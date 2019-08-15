47c7dddb1c423c84fd903498a7b48b338285f5d3
Lisa Caton <lcaton@testsys.com>
2019-08-15 15:31:20 -0400

    currency rate on display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,4 @@
+
 require 'net/http'
 require 'json'
 
@@ -9,9 +10,79 @@ class WelcomeController < ApplicationController
     response_obj = JSON.parse(response)
 
     # Getting a rate
-    @currency = {
-      'USD' => response_obj['rates']['USD'],
-      'IDR' => response_obj['rates']['IDR']
+    @currency = response_obj['rates']
+
+    @currencyCodeToNameHash = {
+      'AED' => 'United Arab Emirates',
+      'ARS' => 'Argentina',
+      'AUD' => 'Australia',
+      'BGN' => 'Bulgaria',
+      'BRL' => 'Brazil',
+      'BSD' => 'Bahamas',
+      'CAD' => 'Canada',
+      'CHF' => 'Switzerland',
+      'CLP' => 'Chile',
+      'CNY' => 'China',
+      'COP' => 'Colombia',
+      'CZK' => 'Czech Republic',
+      'DKK' => 'Denmark',
+      'DOP' => 'Dominican Republic',
+      'EGP' => 'Egypt',
+      'EUR' => 'Germany',
+      'EUR' => 'Austria',
+      'EUR' => 'Belgium',
+      'EUR' => 'Cyprus',
+      'EUR' => 'Estonia',
+      'EUR' => 'Finland',
+      'EUR' => 'France',
+      'EUR' => 'Greece',
+      'EUR' => 'Ireland',
+      'EUR' => 'Italy',
+      'EUR' => 'Latvia',
+      'EUR' => 'Lithuania',
+      'EUR' => 'Luxembourg',
+      'EUR' => 'Malta',
+      'EUR' => 'Netherlands',
+      'EUR' => 'Portugal',
+      'EUR' => 'Slovakia',
+      'EUR' => 'Slovenia',
+      'EUR' => 'Spain',
+      'FJD' => 'Fiji',
+      'GBP' => 'United Kingdom',
+      'GTQ' => 'Guatemala',
+      'HKD' => 'Hong Kong',
+      'HRK' => 'Croatian',
+      'HUF' => 'Hungary',
+      'IDR' => 'Indonesia',
+      'ILS' => 'Israel',
+      'INR' => 'India',
+      'ISK' => 'Iceland',
+      'JPY' => 'Japan',
+      'KRW' => 'Korea',
+      'KZT' => 'Kazakhstan',
+      'MXN' => 'Mexico',
+      'MYR' => 'Malaysia',
+      'NOK' => 'Norway',
+      'NZD' => 'New Zealand',
+      'PAB' => 'Panama',
+      'PEN' => 'Peru',
+      'PHP' => 'Philippines',
+      'PKR' => 'Pakistan',
+      'PLN' => 'Poland',
+      'PYG' => 'Paraguay',
+      'RON' => 'Romania',
+      'RUB' => 'Russian Federation',
+      'SAR' => 'Saudi Arabia',
+      'SEK' => 'Sweden',
+      'SGD' => 'Singapore',
+      'THB' => 'Thailand',
+      'TRY' => 'Turkey',
+      'TWD' => 'Taiwan',
+      'UAH' => 'Ukraine',
+      'USD' => 'United States',
+      'UYU' => 'Uruguay',
+      'VND' => 'Vietnam',
+      'ZAR' => 'South Africa'
     }
 
   end
