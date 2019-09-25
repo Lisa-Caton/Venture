93c6f6214b0ab9627573af519b842bef479839dd
Lisa Caton <lcaton@testsys.com>
2019-09-25 11:04:35 -0400

    Rpsec Passes

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController do
-  describe '#index' do
 
+  describe '#index' do
     it 'tests the url' do
       url = 'https://api.exchangerate-api.com/v4/latest/USD'
       expected = 'https://api.exchangerate-api.com/v4/latest/USD'
@@ -45,17 +45,69 @@ RSpec.describe WelcomeController do
       expect(@currency).to eq expected
     end
 
-    #Testing this line with technical coach on Slack.
     it 'tests that the country-code matches the correct country!' do
-      @currencyCodeToNameHash[AED] = 'United Arab Emirates'
-      expected = 'AED' => 'United Arab Emirates'
-      expect(@currencyCodeToNameHash[AED]).to eq 'AED' => 'United Arab Emirates'
+      @currencyCodeToNameHash = {
+        'AED' => 'United Arab Emirates',
+        'ARS' => 'Argentina',
+        'AUD' => 'Australia',
+        'BGN' => 'Bulgaria',
+        'BRL' => 'Brazil',
+        'BSD' => 'Bahamas',
+        'CAD' => 'Canada',
+        'CHF' => 'Switzerland',
+        'CLP' => 'Chile',
+        'CNY' => 'China',
+        'COP' => 'Colombia',
+        'CZK' => 'Czech Republic',
+        'DKK' => 'Denmark',
+        'DOP' => 'Dominican Republic',
+        'EGP' => 'Egypt',
+        'EUR' => 'Europe',
+        'FJD' => 'Fiji',
+        'GBP' => 'United Kingdom',
+        'GTQ' => 'Guatemala',
+        'HKD' => 'Hong Kong',
+        'HRK' => 'Croatian',
+        'HUF' => 'Hungary',
+        'IDR' => 'Indonesia',
+        'ILS' => 'Israel',
+        'INR' => 'India',
+        'ISK' => 'Iceland',
+        'JPY' => 'Japan',
+        'KRW' => 'Korea',
+        'KZT' => 'Kazakhstan',
+        'MXN' => 'Mexico',
+        'MYR' => 'Malaysia',
+        'NOK' => 'Norway',
+        'NZD' => 'New Zealand',
+        'PAB' => 'Panama',
+        'PEN' => 'Peru',
+        'PHP' => 'Philippines',
+        'PKR' => 'Pakistan',
+        'PLN' => 'Poland',
+        'PYG' => 'Paraguay',
+        'RON' => 'Romania',
+        'RUB' => 'Russian Federation',
+        'SAR' => 'Saudi Arabia',
+        'SEK' => 'Sweden',
+        'SGD' => 'Singapore',
+        'THB' => 'Thailand',
+        'TRY' => 'Turkey',
+        'TWD' => 'Taiwan',
+        'UAH' => 'Ukraine',
+        'USD' => 'United States',
+        'UYU' => 'Uruguay',
+        'VND' => 'Vietnam',
+        'ZAR' => 'South Africa'
+      }
+      @currencyCodeToNameHash['AED'] = 'United Arab Emirates'
+      expected = 'United Arab Emirates'
+      expect(@currencyCodeToNameHash['AED']).to eq expected
     end
-
-
 
   end
 end
+
 
 
 
